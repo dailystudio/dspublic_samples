8e4743658b1d739364fac0fc02be15aa79b079cc
Nan YE <dailystudio2010@gmail.com>
2017-02-09 16:29:01 +0800

    [SimpleNoteRx][INIT]: sample project SimpleNote init;

---
+++ rxSamples/SimpleNoteRx/app/src/main/java/com/dailystudio/simplenoterx/SimpleNoteRxApplication.java
@@ -0,0 +1,16 @@
+package com.dailystudio.simplenoterx;
+
+import com.dailystudio.app.DevBricksApplication;
+
+/**
+ * Created by nanye on 17/2/9.
+ */
+
+public class SimpleNoteRxApplication extends DevBricksApplication {
+
+    @Override
+    protected boolean isDebugBuild() {
+        return BuildConfig.DEBUG;
+    }
+
+}
