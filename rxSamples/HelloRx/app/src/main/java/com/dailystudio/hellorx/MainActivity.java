7c588d2145e9969e617ebd1dcf15eaeb25eaa788
Nan YE <dailystudio2010@gmail.com>
2017-02-20 09:55:55 +0800

    [HelloRx][*]: adjust code style;

--- rxSamples/HelloRx/app/src/main/java/com/dailystudio/hellorx/MainActivity.java
+++ rxSamples/HelloRx/app/src/main/java/com/dailystudio/hellorx/MainActivity.java
@@ -2,7 +2,6 @@ package com.dailystudio.hellorx;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,13 +9,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-import com.dailystudio.development.Logger;
-
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
-import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
@@ -49,69 +45,69 @@ public class MainActivity extends AppCompatActivity {
                     mFab.setClickable(false);
 
                     Observable.intervalRange(0, 11, 0, COUNT_INTERVAL, TimeUnit.MILLISECONDS)
-                            .map(new Function<Long, String>() {
+                    .map(new Function<Long, String>() {
 
-                                @Override
-                                public String apply(Long aLong) throws Exception {
-                                    return String.valueOf(10 - aLong);
-                                }
+                        @Override
+                        public String apply(Long aLong) throws Exception {
+                            return String.valueOf(10 - aLong);
+                        }
 
-                            }).subscribeOn(Schedulers.io())
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .subscribe(new Observer<String>() {
-                                @Override
-                                public void onSubscribe(Disposable d) {
+                    }).subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Observer<String>() {
+                        @Override
+                        public void onSubscribe(Disposable d) {
 
-                                }
+                        }
 
-                                @Override
-                                public void onNext(String value) {
-                                    if (mTextView != null) {
-                                        mTextView.setTextAppearance(MainActivity.this,
-                                                R.style.CounterText);
-                                        mTextView.setText(value);
-                                    }
-                                }
+                        @Override
+                        public void onNext(String value) {
+                            if (mTextView != null) {
+                                mTextView.setTextAppearance(MainActivity.this,
+                                        R.style.CounterText);
+                                mTextView.setText(value);
+                            }
+                        }
 
-                                @Override
-                                public void onError(Throwable e) {
+                        @Override
+                        public void onError(Throwable e) {
 
-                                }
+                        }
 
-                                @Override
-                                public void onComplete() {
-                                    Observable.timer(COUNT_INTERVAL, TimeUnit.MILLISECONDS)
-                                            .subscribeOn(Schedulers.io())
-                                            .observeOn(AndroidSchedulers.mainThread())
-                                            .subscribe(new Observer<Long>() {
-                                                @Override
-                                                public void onSubscribe(Disposable d) {
+                        @Override
+                        public void onComplete() {
+                            Observable.timer(COUNT_INTERVAL, TimeUnit.MILLISECONDS)
+                                    .subscribeOn(Schedulers.io())
+                                    .observeOn(AndroidSchedulers.mainThread())
+                                    .subscribe(new Observer<Long>() {
+                                        @Override
+                                        public void onSubscribe(Disposable d) {
 
-                                                }
+                                        }
 
-                                                @Override
-                                                public void onNext(Long value) {
+                                        @Override
+                                        public void onNext(Long value) {
 
-                                                }
+                                        }
 
-                                                @Override
-                                                public void onError(Throwable e) {
+                                        @Override
+                                        public void onError(Throwable e) {
 
-                                                }
+                                        }
 
-                                                @Override
-                                                public void onComplete() {
-                                                    mFab.setClickable(true);
+                                        @Override
+                                        public void onComplete() {
+                                            mFab.setClickable(true);
 
-                                                    if (mTextView != null) {
-                                                        mTextView.setTextAppearance(MainActivity.this,
-                                                                R.style.CounterCompleteText);
-                                                        mTextView.setText(R.string.hello_rx);
-                                                    }
-                                                }
-                                            });
-                                }
-                            });
+                                            if (mTextView != null) {
+                                                mTextView.setTextAppearance(MainActivity.this,
+                                                        R.style.CounterCompleteText);
+                                                mTextView.setText(R.string.hello_rx);
+                                            }
+                                        }
+                                    });
+                        }
+                    });
                 }
             });
         }
